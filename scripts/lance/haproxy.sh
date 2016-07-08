5a674fb4c3d0a7c044495d8ad928d5c838c37701
Daniel Baur <daniel.baur@uni-ulm.de>
2016-07-08 15:18:26 +0200

    added lance haproxy file

---
+++ scripts/lance/haproxy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MY_DIR="$(dirname "$0")"
+
+
+### main logic ###
+case "$1" in
+  configure)
+        if [ -z ${PUBLIC_LOADBALANCERREQWIKI+123} ] ; then
+                MESSAGE="Environment variable PUBLIC_LOADBALANCERREQWIKI required, but not set."
+                echo $MESSAGE
+                exit 3
+        elif [ -z ${PUBLIC_LOADBALANCERREQWIKI} ] ; then
+                echo "Environment variable PUBLIC_LOADBALANCERREQWIKI required, but not set to reasonable value."
+                exit 3
+        else
+                arr=$(echo $PUBLIC_LOADBALANCERREQWIKI | tr "," "\n")
+                for x in $arr
+                ## take the last one (because there are only one)
+                do
+                        echo "PUBLIC_LOADBALANCERREQWIKI > [$x]"
+                        WIKI_HOSTS+=$(echo "$x" | sed -e "s/:.*$//")
+                        WIKI_HOSTS+=" "
+                done
+        fi
+        ./${MY_DIR}/../shell/haproxy.sh configure $WIKI_HOSTS
+        ;;
+  *)
+        ./${MY_DIR}/../shell/haproxy.sh $@
+esac
