3b61b8d10d9e8868f1ebbc9aaf8b601736ca8bff
Daniel Baur <daniel.baur@uni-ulm.de>
2019-05-03 13:16:21 +0200

    updated lambda

--- lambda/code.js
+++ lambda/code.js
@@ -1,15 +1,70 @@
 'use strict';
 
-const aws = required('aws-sdk');
+const aws = require('aws-sdk');
 var ssm = new aws.SSM();
 
-module.exports.function = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello World',
-    }),
-  };
+const getParam = param => {
+  return new Promise((res, rej) => {
+    ssm.getParameter({
+      Name: param
+    }, (err, data) => {
+      if (err) {
+        return rej(err)
+      }
+      return res(data)
+    })
+})
+}
 
-  callback(null, response);
+const writeParam = (param, value) => {
+  return new Promise((res, rej) => {
+    ssm.putParameter({
+      Name: param,
+      Type: "String",
+      Value: value,
+      Overwrite: true
+    }, (err, data) => {
+      if (err) {
+        return rej(err)
+      }
+      return res(data)
+    })
+})
+}
+
+
+module.exports.function = async (event, context, callback) => {
+
+  console.log("Event: "+JSON.stringify(event));
+  console.log("Context: "+JSON.stringify(context));
+
+  var method = event.httpMethod;
+  var body = event.body;
+
+  if(method === "GET") {
+    const param = await getParam('wordcount')
+    console.log(param);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(param)
+    };
+  } else if (method === "POST") {
+
+
+    var parsed = JSON.parse(body);
+
+    const write = await writeParam('wordcount',parsed.value)
+    console.log(write);
+    const read = await getParam('wordcount')
+    return {
+      statusCode: 200,
+      body: JSON.stringify(read)
+    };
+  } else {
+    return {
+      statusCode: 501,
+      body: JSON.stringify("Request method "+method+" is not supported.")
+    };
+  }
 };
+
