0711bc3c821858ca7b33901146cebb8a3854f929
Daniel Baur <dbaur@users.noreply.github.com>
2019-11-18 11:29:39 +0100

    Update communicationTest.sh

--- scripts/lance/communicationTest.sh
+++ scripts/lance/communicationTest.sh
@@ -1,1 +1,17 @@
-
+DB_HOST="0.0.0.0"
+if [ -z ${PUBLIC_LANCEREQUIRESFAAS+123} ] ; then
+        MESSAGE="Environment variable PUBLIC_LANCEREQUIRESFAAS required, but not set."
+        echo $MESSAGE
+        exit 3
+elif [ -z ${PUBLIC_LANCEREQUIRESFAAS} ] ; then
+        echo "Environment variable LANCEREQUIRESFAAS required, but not set to reasonable value."
+        exit 3
+else
+        arr=$(echo $PUBLIC_LANCEREQUIRESFAAS | tr "," "\n")
+        for x in $arr
+        ## take the last one (because there are only one)
+        do
+                echo "PUBLIC_LANCEREQUIRESFAAS > [$x]"
+                DB_HOST=$(echo "$x" | sed -e "s/:.*$//")
+        done
+fi
