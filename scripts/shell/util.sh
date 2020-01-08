a09cff9f45a5c2ffee7007ee78f17d0b5c7a6957
Daniel Baur <dbaur@users.noreply.github.com>
2020-01-08 11:29:42 +0100

    Update util.sh

--- scripts/shell/util.sh
+++ scripts/shell/util.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 apt_update() {
-unset UCF_FORCE_CONFFOLD
-export UCF_FORCE_CONFFNEW=YES
-ucf --purge /boot/grub/menu.lst
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -o Dpkg::Options::="--force-confold" --force-yes -fuy dist-upgrade
+apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" dist-upgrade
 }
